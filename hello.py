2008b21b3112a890ca4ec6ddc789e1454007fa20
Alejo Garcia Galan <alejogarciagalan@gmail.com>
2022-01-18 08:37:29 +0100

    envio final

--- hello.py
+++ hello.py
@@ -30,23 +30,25 @@ def create_file():
                 answer =  read_input_val("Introduce the answer: ", "Answer cannot be 'exit'.", [], ["exit"])
                 writer.writerow({'question': question, 'answer': answer})
             else:
-                print("Saving new file(", name, ") ...")
+                print("Saving new file(" + name + ") ...")
                 break
-        file.close()
 
             
 def use_file():
     name = input("Input file name: ") + ".csv"
     correct_list = []
     failed_list = []
+
     try:
         with open(name, newline='') as file:
             print("Opening file '" + name + "'...")
             for line in csv.reader(file):
                 failed_list.append(tuple(line))
+            failed_list.pop(0)
     except Exception:
         print("Failed to open file. File may not exist.")
-        #return
+        return
+
     if read_input_val("Which mode of study you want to use? [n-options: nopt, quickcard: qcard]: ", "Wrong input.", ["nopt", "qcard"], []) \
             == "nopt":
         number_opt = int(input("How many options?: "))
@@ -55,56 +57,52 @@ def use_file():
             number_opt = int(input("How many options?: "))
     else:
         number_opt = 1
-    number = int(input("Continious mode or 'x' questions? [continious: 0, xquest: n]: ")) 
-    while number < 0:
-        print("Wrong input. Must be positive or zero.")
-        number = int(input("Continious mode or 'x' questions? [continious: 0, xquest: n]: ")) 
-    if number == 0:
-        while failed_list != []:
-            quest = do_question(number_opt, failed_list, correct_list)
-            read_input_val("To se solution pulse enter: ", "Wrong input.", [""], [])
-            print("SOLUTION:")
-            solution = failed_list[quest]
-            print(solution[1])
-            res = read_input_val("Was right?: [y/n]", "Wrong input.", ["y", "n"], [])
-            if res == "yes":
-                print("Congratulations!!!")
-                correct_list.append(failed_list.pop(quest))
-            else:
-                print("You'll get it next time")
-            if read_input_val("Want to do another?: [y/n]", "Wrong input.", ["y", "n"], []) == "n":
-                break
-    else:
-        n = number
-        correct_answers = 0
-        while failed_list != [] and n > 0:
-            quest = do_question(number_opt, failed_list, correct_list)
-            read_input_val("To se solution pulse enter: ", "Wrong input.", [""], [])
-            print("SOLUTION:")
-            print(failed_list[quest][2])
-            res = read_input_val("Was right?: [y/n]", "Wrong input.", ["y", "n"], [])
-            if res == "yes":
-                print("Congratulations!!!")
-                correct_list.append(failed_list.pop(quest))
-            else:
-                print("You'll get it next time")
-        print("You got", correct_answers, "answers correct of", number + ".")
 
-def do_question(options, list_questions, list2):
-    number = random.randint(0, len(list_questions))
-    print("QUESTION:")
-    question = list_questions[number]
-    print(question[0])
+    while (number := int(input("Continious mode or 'x' questions? [continious: 0, xquest: n]: "))) < 0:
+        print("Wrong input. Must be positive or zero.")
+    n = 1
+    correct_answers = 0     
+    res = "" 
+    print("el numero de preguntas es", number)
+
+    while failed_list != [] and res != "exit":
+        quest = do_question(n, number_opt, failed_list, correct_list)
+        read_input_val("To see solution pulse enter : ", "Wrong input.", [""], [])
+        print("SOLUTION:")
+        print(failed_list[quest][1])
+        res = read_input_val("Was right? (to exit write 'exit') [y/n/exit]: ", "Wrong input.", ["y", "n", "exit"], [])
+        if res == "y":
+            print("Congratulations!!!")
+            correct_answers += 1
+            correct_list.append(failed_list.pop(quest))
+        else:
+            print("You'll get it next time")
+        n += 1
+        if number > 0 and n > number:
+            break
+    print("You got", correct_answers, "answers correct of", number)
+
+def do_question(number, options, list_questions, list2):
+    print("QUESTION " + str(number) + ":")
+    number = random.randrange(0, len(list_questions))
+    print(list_questions[number][0])
     if options > 1:
-        long_list = list_questions + list2
-        option_list = []
-        option_list.append(list_questions[number])
+        if options > len(list_questions):
+            options = len(list_questions)
+        option_list =[]
+        option_list.append(list_questions[number][1])
+        long_list = []
+        for i in list_questions + list2:
+            long_list.append(i[1])
         for i in range(1, options):
-            option_list.append(long_list.pop(random.randint(0,len(long_list))))
+            option = option_list[0]
+            while option in option_list: 
+                option = long_list.pop(random.randrange(0, len(long_list)))
+            option_list.append(option)
         random.shuffle(option_list)
         print("OPTIONS:")
         for i in option_list:
-            print(" -> " + i[1])
+            print(" -> " + i)
     return number
 
 # Ask for action to take
